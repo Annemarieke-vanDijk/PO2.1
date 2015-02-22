3b3553698adc1518acf7b05d1cce080f12fa98fc
HorseWoman99 <annemarieke.van.dijk.99@gmail.com>
2015-02-22 12:29:26 +0100

    some minor fixes

--- inkomen.js
+++ inkomen.js
@@ -1,6 +1,8 @@
-function income(uurloon, uren, minuten)
+function incomeCalc(uurloon, uren, minuten) {
 	var uurloon = document.getElementById("uurloon");
 	var uren = document.getElementById("uren");
 	var minuten = document.getElementById("minuten");
 
-	var income = uurloon * (uren + minuten*1/60)
+	var income = uurloon * (uren + minuten*1/60);
+	alert(income);
+}
