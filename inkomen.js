2f9261e332588f06ac1a38df8107a46227caa465
HorseWoman99 <annemarieke.van.dijk.99@gmail.com>
2015-03-01 20:21:30 +0100

    Some bug, i didn't see. Fixed. (a bug in the income calculator)

--- inkomen.js
+++ inkomen.js
@@ -3,6 +3,9 @@ function incomeCalc() {
 	var uren = document.getElementById("uren").value;
 	var minuten = document.getElementById("minuten").value;
 
-	var income = uurloon * (uren+minuten*1/60);
+	uren = parseInt(uren);
+	minuten = parseInt(minuten);
+
+	var income = uurloon * (uren + minuten / 60);
 	alert('Uw inkomsten bedragen €' + income.toFixed(2));
 }
